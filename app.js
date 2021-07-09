9776ad5e72fa844ba7e27d10e2099e023c96b3a3
ijh0614 <63185555+ijh0614@users.noreply.github.com>
2021-07-09 14:55:32 +0900

    inputName_start

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const age = parseInt(prompt("how old are you?"));
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-console.log(isNaN(age));
+function handleLoginBtnClick(){
+    const value = loginInput.value;
+    if(value === ""){
+        console.log("please input your name");
+    } 
+    else if(value.length < 10) {
+        console.log(value.length);
+    }
+}
+
+loginButton.addEventListener("click", handleLoginBtnClick);
