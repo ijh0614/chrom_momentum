adb4c892776420b0ef33813077927ebf190987ba
ijh0614 <63185555+ijh0614@users.noreply.github.com>
2021-07-29 18:10:02 +0900

    make
    quotes.js file

---
+++ js/quotes.js
@@ -0,0 +1,42 @@
+const quotes = [
+{
+    quote = "일은 망해도 나는 망하지 않는다.",
+    author = "우동이즘"
+},
+{
+    quote = "인구 지진이 예고되어 있다.",
+    author = "슈카"
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+{
+    quote = "",
+    author = ""
+},
+];
